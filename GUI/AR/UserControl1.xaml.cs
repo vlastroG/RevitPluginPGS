2cacac6d5c0cf629ed363ac0987d59dc021e874e
Владимир Строганов <vlastroganov@gmail.com>
2022-04-21 15:54:54 +0300

    Добавление окна ввода для команды RoomsArea

---
+++ GUI/AR/UserControl1.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MS.GUI.AR
+{
+    /// <summary>
+    /// Interaction logic for UserControl1.xaml
+    /// </summary>
+    public partial class InputRoomsArea : Window
+    {
+        public InputRoomsArea()
+        {
+            InitializeComponent();
+        }
+
+        public bool AllProjCalc { get; private set; }
+
+        public string RevitTemplate { get; private set; }
+
+        public int AreaRound { get; private set; }
+
+        /*--------------------template_choice-----------------------------*/
+
+        private void tempPGS_Checked(object sender, RoutedEventArgs e)
+        {
+            RevitTemplate = "PGS";
+        }
+
+        private void tempADSK_Checked(object sender, RoutedEventArgs e)
+        {
+            RevitTemplate = "ADSK";
+        }
+
+        /*--------------------calc_scale_choice---------------------------*/
+
+        private void allProj_Checked(object sender, RoutedEventArgs e)
+        {
+            AllProjCalc = true;
+        }
+
+        private void allView_Checked(object sender, RoutedEventArgs e)
+        {
+            AllProjCalc = false;
+        }
+
+        /*--------------------round_choice-------------------------------*/
+
+        private void twoDecimal_Checked(object sender, RoutedEventArgs e)
+        {
+            AreaRound = 2;
+        }
+
+        private void threeDecimal_Checked(object sender, RoutedEventArgs e)
+        {
+            AreaRound = 3;
+        }
+    }
+}
