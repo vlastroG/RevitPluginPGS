32ccb2726063780992c5ccd903a037c4f0eeffab
Владимир Строганов <vlastroganov@gmail.com>
2022-04-22 09:44:05 +0300

    Тест округления decimal

--- TestConsole/Program.cs
+++ TestConsole/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿double decValue = 177.288;
+double decValue1 = 177.5555;
+var rDec = Math.Round(decValue, 2);
+var rDec1 = Math.Round(decValue1, 2);
+var sum = rDec + rDec1;
+
+Console.WriteLine(rDec);
+Console.WriteLine(rDec1);
+Console.WriteLine(sum);
+Console.ReadLine();
