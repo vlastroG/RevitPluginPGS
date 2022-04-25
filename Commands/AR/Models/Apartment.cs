4c7d67e3b9b783124e17cd452a02538cceedd186
Владимир Строганов <vlastroganov@gmail.com>
2022-04-25 11:00:16 +0300

    Добавление класса квартиры.

---
+++ Commands/AR/Models/Apartment.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MS.Commands.AR.Models
+{
+    /// <summary>
+    /// Квартира
+    /// </summary>
+    internal class Apartment
+    {
+        /// <summary>
+        /// Номер квартиры
+        /// </summary>
+        public string Number { get; set; }
+
+        /// <summary>
+        /// Количество жилых комнат
+        /// </summary>
+        public int LivingRoomsCount { get; set; }
+
+        /// <summary>
+        /// Отпаливаемая площадь
+        /// </summary>
+        public double AreaHeateed { get; set; }
+
+        /// <summary>
+        /// Жилая площадь
+        /// </summary>
+        public double AreaLiving { get; set; }
+
+        /// <summary>
+        /// Приведенная площадь квартиры (с учетом площади неотапливаемых помещений с коэффициентом).
+        /// </summary>
+        public double AreaTotalCoeff { get; set; }
+
+        /// <summary>
+        /// Общая площадь квартиры без коэффициентов
+        /// </summary>
+        private double AreaTotal { get; set; }   
+    }
+}
