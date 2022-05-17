002c06a2a869ad80337c447fe1c534e5e2651609
Владимир Строганов <vlastroganov@gmail.com>
2022-05-17 11:23:52 +0300

    Получение высоты и ширины каждого проема в стене.

--- Commands/AR/MasonryMesh.cs
+++ Commands/AR/MasonryMesh.cs
@@ -10,6 +10,8 @@ namespace MS.Commands.AR
     [Regeneration(RegenerationOption.Manual)]
     public class MasonryMesh : IExternalCommand
     {
+        private double _mm_in_foot = 304.8;
+
         public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
         {
             Document doc = commandData.Application.ActiveUIDocument.Document;
@@ -17,7 +19,10 @@ namespace MS.Commands.AR
 
             //string par_name__MeshRowCount = "Арм.ОчереднАрмирРядовКладки";//Определить параметр для фильтрации
 
-            Guid guid = Guid.Parse("42795f38-352d-44c6-b739-4a97d0f765db");// Guid параметра Арм.ОчереднАрмирРядовКладки
+            Guid guid_par_mesh_rows_count = Guid.Parse("42795f38-352d-44c6-b739-4a97d0f765db");// Guid параметра Арм.ОчереднАрмирРядовКладки
+            Guid guid_par_width = Guid.Parse("8f2e4f93-9472-4941-a65d-0ac468fd6a5d");// Guid параметра Рзм.Ширина
+            Guid guid_par_height = Guid.Parse("da753fe3-ecfa-465b-9a2c-02f55d0c2ff1");// Guid параметра Рзм.Высота
+
             // Выбор всех однослойных стен в проекте, у которых значение параметра КоличествоАрмируемыхРядов >= 1.
             var filter = new FilteredElementCollector(doc);
             var walls = filter
@@ -26,16 +31,29 @@ namespace MS.Commands.AR
                 .ToElements()
                 .Select(e => e as Wall)
                 .Where(w => w.WallType.GetCompoundStructure().LayerCount == 1)
-                .Where(w => w.get_Parameter(guid).HasValue == true && w.get_Parameter(guid).AsDouble() >= 1);
+                .Where(w => w.get_Parameter(guid_par_mesh_rows_count).HasValue == true && w.get_Parameter(guid_par_mesh_rows_count).AsDouble() >= 1);
 
             foreach (var wall in walls)
             {
                 var list_openings = wall
                     .FindInserts(true, true, true, true)
-                    .Select(i => doc.GetElement(i))
+                    .Select(i => doc.GetElement(i) as FamilyInstance)
                     .ToList();
+                foreach (var opening in list_openings)
+                {
+                    var width = opening.get_Parameter(guid_par_width).AsDouble() * _mm_in_foot;
+                    if (width == 0)
+                    {
+                        width = opening.Symbol.get_Parameter(guid_par_width).AsDouble() * _mm_in_foot;
+                    }
 
-                var door = list_openings[0];
+                    var height = opening.get_Parameter(guid_par_height).AsDouble() * _mm_in_foot;
+                    if (height == 0)
+                    {
+                        height = opening.Symbol.get_Parameter(guid_par_height).AsDouble() * _mm_in_foot;
+                    }
+                }
+
             }
 
 
