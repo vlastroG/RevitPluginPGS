998a395a56bbd9169ffcd6e58ad7ff79fa96e784
Владимир Строганов <vlastroganov@gmail.com>
2022-05-17 18:04:28 +0300

    Workday End

--- Commands/MEP/PipelineFittings.cs
+++ Commands/MEP/PipelineFittings.cs
@@ -15,7 +15,26 @@ namespace MS.Commands.MEP
     {
         public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
         {
-            throw new NotImplementedException();
+
+
+            Document doc = commandData.Application.ActiveUIDocument.Document;
+            UIDocument uidoc = commandData.Application.ActiveUIDocument;
+
+            var filter = new FilteredElementCollector(doc);
+
+            var pipe_accessories = filter
+                .OfCategory(BuiltInCategory.OST_PipeAccessory)
+                .WhereElementIsNotElementType()
+                .ToElements()
+                .Select(e => e as FamilyInstance);
+
+
+            foreach (var pipe_acc in pipe_accessories)
+            {
+                var t = pipe_acc;
+            }
+
+            return Result.Succeeded;
         }
     }
 }
