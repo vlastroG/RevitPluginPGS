8e976a8ab7b02f3e3b03ed61f8043979d9a0b56d
Владимир Строганов <vlastroganov@gmail.com>
2022-05-20 11:46:28 +0300

    Изменение абсолютных путей к изображениям на относительные

--- Main.cs
+++ Main.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Reflection;
 using System.Windows.Media.Imaging;
+using MS.Utilites;
 
 
 namespace MS
@@ -31,6 +32,8 @@ namespace MS
             // Create Ribbon tab
             var path = Assembly.GetExecutingAssembly().Location;
 
+            var assembly_dir = WorkWithPath.AssemblyDirectory;
+
             // Создание вкладки в ленте
             application.CreateRibbonTab("PGS-BIM");
             // Инфо раздел
@@ -46,47 +49,47 @@ namespace MS
 
             // Info command
             PushButtonData btnInfo = new PushButtonData("Info", "Info", path, "MS.Info");
-            Uri btnInfoImagePath = new Uri(@"D:\Строганов В.Г\REVIT\! C#_Plug-ins\MS\Images\Icons\General\Info.png");
+            Uri btnInfoImagePath = new Uri(assembly_dir + @"\Images\Icons\General\Info.png");
             BitmapImage btnInfoImage = new BitmapImage(btnInfoImagePath);
             btnInfo.LargeImage = btnInfoImage;
             btnInfo.ToolTip = "Выбор элементов по заданной категории. Категория берется из первого выбранного элемента, затем рамкой выбираются все элементы заданной категории";
 
             // Selection command
             PushButtonData btnSelection = new PushButtonData("Selection", "Выбор\nпомещений", path, "MS.Selector");
-            Uri btnSelectionImagePath = new Uri(@"D:\Строганов В.Г\REVIT\! C#_Plug-ins\MS\Images\Icons\General\Selection.png");
+            Uri btnSelectionImagePath = new Uri(assembly_dir + @"\Images\Icons\General\Selection.png");
             BitmapImage btnSelectionImage = new BitmapImage(btnSelectionImagePath);
             btnSelection.LargeImage = btnSelectionImage;
 
             // MasonryMesh command
             PushButtonData btnMasonryMesh = new PushButtonData("MasonryMesh", "Кладочная\nсетка", path, "MS.Commands.AR.MasonryMesh");
-            Uri btnMasonryMeshImagePath = new Uri(@"D:\Строганов В.Г\REVIT\! C#_Plug-ins\MS\Images\Icons\General\Selection.png");
+            Uri btnMasonryMeshImagePath = new Uri(assembly_dir + @"\Images\Icons\General\Selection.png");
             BitmapImage btnMasonryMeshImage = new BitmapImage(btnMasonryMeshImagePath);
             btnMasonryMesh.LargeImage = btnMasonryMeshImage;
 
 
             // RoomsAreaPGS command
             PushButtonData btnRmArea = new PushButtonData("RoomsAreaNew", "Квартирография\nPGS_temp", path, "MS.Commands.AR.RoomsAreaNew");
-            Uri btnRmAreaImagePath = new Uri(@"D:\Строганов В.Г\REVIT\! C#_Plug-ins\MS\Images\Icons\AR\RoomsArea.png");
+            Uri btnRmAreaImagePath = new Uri(assembly_dir + @"\Images\Icons\AR\RoomsArea.png");
             BitmapImage btnRmAreaImage = new BitmapImage(btnRmAreaImagePath);
             btnRmArea.LargeImage = btnRmAreaImage;
 
 
             // RoomsFinishingCommand
             PushButtonData btnRmFinishing = new PushButtonData("RoomsFinishing", "Отделка", path, "MS.RoomsFinishing");
-            Uri btnRmFinishingImagePath = new Uri(@"D:\Строганов В.Г\REVIT\! C#_Plug-ins\MS\Images\Icons\AR\RoomsFinishing.png");
+            Uri btnRmFinishingImagePath = new Uri(assembly_dir + @"\Images\Icons\AR\RoomsFinishing.png");
             BitmapImage btnRmFinishingImage = new BitmapImage(btnRmFinishingImagePath);
             btnRmFinishing.LargeImage = btnRmFinishingImage;
 
 
             // SS Numerator command
             PushButtonData btnNumerator = new PushButtonData("Numerator", "Маркировка", path, "MS.Numerator");
-            Uri btnNumeratorImagePath = new Uri(@"D:\Строганов В.Г\REVIT\! C#_Plug-ins\MS\Images\Icons\SS\СС.png");
+            Uri btnNumeratorImagePath = new Uri(assembly_dir + @"\Images\Icons\SS\СС.png");
             BitmapImage btnNumeratorImage = new BitmapImage(btnNumeratorImagePath);
             btnNumerator.LargeImage = btnNumeratorImage;
 
             // ОВиК Numerator command
             PushButtonData btnPipelineFittings = new PushButtonData("PipelineFittings", "Арматура\nтрубопроводов", path, "MS.Commands.MEP.PipelineFittings");
-            Uri btnPipelineFittingsImagePath = new Uri(@"D:\Строганов В.Г\REVIT\! C#_Plug-ins\MS\Images\Icons\General\Info.png");
+            Uri btnPipelineFittingsImagePath = new Uri(assembly_dir + @"\Images\Icons\General\Info.png");
             BitmapImage btnPipelineFittingsImage = new BitmapImage(btnPipelineFittingsImagePath);
             btnPipelineFittings.LargeImage = btnPipelineFittingsImage;
 
