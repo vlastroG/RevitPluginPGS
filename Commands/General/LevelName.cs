dc3634d28dada6d081e0fd5822d0848fd5285d5c
Владимир Строганов <vlastroganov@gmail.com>
2022-05-20 16:52:27 +0300

    Добавление класса для команды добавления отметок в имена уровней

---
+++ Commands/General/LevelName.cs
@@ -0,0 +1,25 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MS.Commands.General
+{
+    [Transaction(TransactionMode.Manual)]
+    [Regeneration(RegenerationOption.Manual)]
+    public class LevelName : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            Document doc = commandData.Application.ActiveUIDocument.Document;
+
+
+
+            return Result.Succeeded;
+        }
+    }
+}
