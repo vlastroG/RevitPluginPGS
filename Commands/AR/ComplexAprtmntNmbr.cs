a17e1a8ec32bd2985d5520685f040202a3cd0124
Владимир Строганов <vlastroganov@gmail.com>
2022-05-20 15:41:20 +0300

    Реализация команды для назначения составного номера квартиры

--- Commands/AR/ComplexAprtmntNmbr.cs
+++ Commands/AR/ComplexAprtmntNmbr.cs
@@ -28,10 +28,57 @@ namespace MS.Commands.AR
         /// </summary>
         private readonly Guid guid_par_apartment_index = Guid.Parse("a2985e5c-b28e-416a-acf6-7ab7e4ee6d86");
 
+        /// <summary>
+        /// Guid параметра СоставнойНомерКвартиры
+        /// </summary>
+        private readonly BuiltInParameter guid_par_cmplx_aprtm_num = BuiltInParameter.ALL_MODEL_INSTANCE_COMMENTS;
+        //private readonly Guid guid_par_cmplx_aprtm_num = Guid.Parse("");
 
+
+        /// <summary>
+        /// Заполнение составного параметра номера квартиры
+        /// Сейчас составной параметр записавается в Комментарии.
+        /// Чтобы записывалось в общий параметр надо поменять поле guid_par_cmplx_aprtm_num
+        /// </summary>
+        /// <param name="commandData"></param>
+        /// <param name="message"></param>
+        /// <param name="elements"></param>
+        /// <returns></returns>
+        /// <exception cref="NotImplementedException"></exception>
         public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
         {
-            throw new NotImplementedException();
+            Document doc = commandData.Application.ActiveUIDocument.Document;
+            UIDocument uidoc = commandData.Application.ActiveUIDocument;
+
+            var rooms_filter = new FilteredElementCollector(doc);
+            var rooms = rooms_filter
+                .OfCategory(BuiltInCategory.OST_Rooms)
+                .WhereElementIsNotElementType()
+                .ToElements();
+
+            using (Transaction trans = new Transaction(doc))
+            {
+                trans.Start("Составные номера квартир");
+
+                foreach (var room in rooms)
+                {
+                    StringBuilder sb = new StringBuilder();
+                    sb.Append(room.get_Parameter(guid_par_liv_rooms_count).AsValueString());
+                    sb.Append(room.get_Parameter(guid_par_apartment_type).AsValueString());
+                    sb.Append(room.get_Parameter(guid_par_apartment_index).AsValueString());
+
+                    var cmplx_aprtm_num = sb.ToString();
+                    if (cmplx_aprtm_num == "0")
+                    {
+                        cmplx_aprtm_num = String.Empty;
+                    }
+                    room.get_Parameter(guid_par_cmplx_aprtm_num).Set(cmplx_aprtm_num);
+                }
+
+                trans.Commit();
+            }
+
+            return Result.Succeeded;
         }
     }
 }
