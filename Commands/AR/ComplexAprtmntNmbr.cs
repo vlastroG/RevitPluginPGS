6166be184268117bb4be48de92f6d29621349e76
Владимир Строганов <vlastroganov@gmail.com>
2022-05-20 14:45:13 +0300

    Создание класса для составной нумерации квартир.

---
+++ Commands/AR/ComplexAprtmntNmbr.cs
@@ -0,0 +1,20 @@
+﻿using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MS.Commands.AR
+{
+    [Transacti
+    [Regeneration(RegenerationOption.Manual)]
+    public class ComplexAprtmntNmbr : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- Main.cs
+++ Main.cs
@@ -54,7 +54,7 @@ namespace MS
             btnInfo.LargeImage = btnInfoImage;
             btnInfo.ToolTip = "Выбор элементов по заданной категории. Категория берется из первого выбранного элемента, затем рамкой выбираются все элементы заданной категории";
 
-            // Selection command
+            // SelectionRooms command
             PushButtonData btnSelection = new PushButtonData("Selection", "Выбор\nпомещений", path, "MS.Selector");
             Uri btnSelectionImagePath = new Uri(assembly_dir + @"\Images\Icons\General\Selection.png");
             BitmapImage btnSelectionImage = new BitmapImage(btnSelectionImagePath);
@@ -72,6 +72,12 @@ namespace MS
             Uri btnRmAreaImagePath = new Uri(assembly_dir + @"\Images\Icons\AR\RoomsArea.png");
             BitmapImage btnRmAreaImage = new BitmapImage(btnRmAreaImagePath);
             btnRmArea.LargeImage = btnRmAreaImage;
+
+            // ComplexApartmentNumber command
+            PushButtonData btnCmplxAprtNum = new PushButtonData("ComplexAprtmntNmbr", "Составные номера\nквартир", path, "MS.Commands.AR.ComplexAprtmntNmbr");
+            Uri btnCmplxAprtNumImagePath = new Uri(assembly_dir + @"\Images\Icons\General\Info.png");
+            BitmapImage btnCmplxAprtNumImage = new BitmapImage(btnCmplxAprtNumImagePath);
+            btnCmplxAprtNum.LargeImage = btnCmplxAprtNumImage;
 
 
             // RoomsFinishingCommand
@@ -103,10 +109,10 @@ namespace MS
 
             // General panel
             panelGeneral.AddItem(btnInfo);
-            //panelGeneral.AddItem(btnSelection);
 
             // AR panel
             panelAR.AddItem(btnRmArea);
+            panelAR.AddItem(btnCmplxAprtNum);
             //panelAR.AddItem(btnRmFinishing);
             panelAR.AddItem(btnSelection);
             panelAR.AddItem(btnMasonryMesh);
