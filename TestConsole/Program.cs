ff64e01d40c8111b164430014b76349b9b1968a8
Владимир Строганов <vlastroganov@gmail.com>
2022-05-23 10:10:15 +0300

    Написать формат названия уровня
    Определить паттерн строки для проверки названия уровня

--- TestConsole/Program.cs
+++ TestConsole/Program.cs
@@ -2,9 +2,29 @@
 
 using System.Text.RegularExpressions;
 
-Regex regex = new Regex(@"\(.*\d+\.\d+\)$");
-string test = "Этаж 1 (+5.000)";
+Regex regex = new Regex(@"^[А-Я][а-я]+\s+\+*\-*\d*\s*\(\+*\-*\d+\.\d+\)$");
+string test = "Этаж -1 (+5.000)";
+string test1 = "Этаж +1 (+5.000)";
+string test2 = "Эывпа таж 1 (5.954)";
+string test3 = "Этаж 1 (+.000)";
+string test4 = "Этаж 13232 (+5.0)";
+string test5 = "Этаж 1(+5.000)";
+string test6 = "Этаж1 (+5.123)";
+string test7 = "Этупр54ен54нж 1 (5.00)";
+string test8 = "Кровля (+15.000)";
+string test9 = "Этаж 1";
+string test10 = "Этаж 1 (+125.000)";
 
 
-Console.WriteLine(regex.IsMatch(test));
+Console.WriteLine(test + " - " + regex.IsMatch(test));
+Console.WriteLine(test1 + " - " + regex.IsMatch(test1));
+Console.WriteLine(test2 + " - " + regex.IsMatch(test2));
+Console.WriteLine(test3 + " - " + regex.IsMatch(test3));
+Console.WriteLine(test4 + " - " + regex.IsMatch(test4));
+Console.WriteLine(test5 + " - " + regex.IsMatch(test5));
+Console.WriteLine(test6 + " - " + regex.IsMatch(test6));
+Console.WriteLine(test7 + " - " + regex.IsMatch(test7));
+Console.WriteLine(test8 + " - " + regex.IsMatch(test8));
+Console.WriteLine(test9 + " - " + regex.IsMatch(test9));
+Console.WriteLine(test10 + " - " + regex.IsMatch(test10));
 Console.ReadLine();
