5eb1a81b8417fb5f73bf91e85d6798e64d905a3f
Владимир Строганов <vlastroganov@gmail.com>
2022-05-31 11:51:25 +0300

    Расчет площадей проемов помещений
    Расчет всех окон, дверей и витражей, образующих
    границу помещения

--- Commands/AR/OpeningsArea.cs
+++ Commands/AR/OpeningsArea.cs
@@ -6,8 +6,6 @@ using MS.Utilites;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace MS.Commands.AR
 {
@@ -26,10 +24,9 @@ namespace MS.Commands.AR
         private static readonly string _phase = "Новая конструкция";
 
         /// <summary>
-        /// Словарь Id помещений и площадей проемов, выполненных загружаемыми семействами
+        /// Опции подсчета сегментов границ помещений
         /// </summary>
-        private static readonly Dictionary<ElementId, double> _dict_roomId_openingsArea = new Dictionary<ElementId, double>();
-
+        private static readonly SpatialElementBoundaryOptions _boundaryOptions = new SpatialElementBoundaryOptions();
 
 
         public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
@@ -38,12 +35,14 @@ namespace MS.Commands.AR
             UIDocument uidoc = uiapp.ActiveUIDocument;
             Document doc = uidoc.Document;
 
+            Dictionary<ElementId, double> _dict_roomId_openingsArea = new Dictionary<ElementId, double>();
+
             var filter_rooms = new FilteredElementCollector(doc);
             var rooms = filter_rooms
                 .OfCategory(BuiltInCategory.OST_Rooms)
                 .WhereElementIsNotElementType()
                 .ToElements()
-                .Where(r => r.get_Parameter(BuiltInParameter.PHASE_CREATED).AsValueString() == _phase)
+                .Where(r => r.get_Parameter(BuiltInParameter.ROOM_PHASE).AsValueString() == _phase)
                 .Where(r => (r as Room).Area > 0)
                 .ToArray();
 
@@ -62,23 +61,59 @@ namespace MS.Commands.AR
                 .Where(d => d.get_Parameter(BuiltInParameter.PHASE_CREATED).AsValueString() == _phase);
 
             var openings = doors.Concat(windows);
-            var phaseOfRooms = doc.GetElement(rooms.FirstOrDefault().get_Parameter(BuiltInParameter.PHASE_CREATED).AsElementId()) as Phase;
+            var phaseOfRooms = doc.GetElement(rooms.FirstOrDefault().get_Parameter(BuiltInParameter.ROOM_PHASE).AsElementId()) as Phase;
 
             foreach (var opening in openings)
             {
                 var opening_fam_inst = opening as FamilyInstance;
                 var opening_area = WorkWithGeometry.GetOpeningArea(doc, opening_fam_inst);
-                _dict_roomId_openingsArea.MapIncrease(opening_fam_inst.get_FromRoom(phaseOfRooms).Id, opening_area);
-                _dict_roomId_openingsArea.MapIncrease(opening_fam_inst.get_ToRoom(phaseOfRooms).Id, opening_area);
+
+                var from_room = opening_fam_inst.get_FromRoom(phaseOfRooms);
+                var to_room = opening_fam_inst.get_ToRoom(phaseOfRooms);
+                if (from_room != null)
+                {
+                    _dict_roomId_openingsArea.MapIncrease(from_room.Id, opening_area);
+                }
+                if (to_room != null)
+                {
+                    _dict_roomId_openingsArea.MapIncrease(to_room.Id, opening_area);
+                }
             }
 
-            foreach (var room in rooms)
+            using (Transaction trans = new Transaction(doc))
             {
-                double room_area = 0;
-                if (_dict_roomId_openingsArea.ContainsKey(room.Id))
+                trans.Start("Площади проемов");
+
+                foreach (Room room in rooms)
                 {
-                    room_area = _dict_roomId_openingsArea[room.Id];
+                    double room_area = 0;
+                    if (_dict_roomId_openingsArea.ContainsKey(room.Id))
+                    {
+                        room_area = _dict_roomId_openingsArea[room.Id];
+                    }
+
+                    var circuits = room.GetBoundarySegments(_boundaryOptions);
+                    foreach (var circuit in circuits)
+                    {
+                        foreach (var bound in circuit)
+                        {
+                            var el_bound = doc.GetElement(bound.ElementId);
+                            if (el_bound != null)
+                            {
+                                var el_bound_type_name = el_bound.GetType().Name;
+                                if (el_bound_type_name == "ModelLine"
+                                    || (el_bound is Wall
+                                    && (el_bound as Wall).CurtainGrid != null))
+                                {
+                                    room_area += (room.UnboundedHeight) * (bound.GetCurve().Length);
+                                }
+                            }
+                        }
+                    }
+                    room.get_Parameter(_parOpeningsArea).Set(room_area);
                 }
+
+                trans.Commit();
             }
 
             return Result.Succeeded;
