1b0f6ac4f88d0b81584c15d75fc5ed0e936097a7
Владимир Строганов <vlastroganov@gmail.com>
2022-06-07 17:28:14 +0300

    Создать обобщенный класс фильтра элементов

---
+++ Utilites/SelectionFilterElementsOfClass.cs
@@ -0,0 +1,27 @@
+﻿using Autodesk.Revit.DB;
+using Autodesk.Revit.UI.Selection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MS.Utilites
+{
+    /// <summary>
+    /// Фильтр выбора элементов по заданному классу Revit API
+    /// </summary>
+    /// <typeparam name="T">Заданный класс Revit</typeparam>
+    public class SelectionFilterElementsOfClass<T> : ISelectionFilter
+    {
+        public bool AllowElement(Element elem)
+        {
+            return elem is T;
+        }
+
+        public bool AllowReference(Reference reference, XYZ position)
+        {
+            return true;
+        }
+    }
+}
