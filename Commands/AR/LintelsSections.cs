846f9ee9a0ee17ed07e7490aea11b120a2627d5a
Vladimir Stroganov <vlastroganov@gmail.com>
2022-07-21 18:04:35 +0300

    Создать заготовку для разрезов по перемычкам

---
+++ Commands/AR/LintelsSections.cs
@@ -0,0 +1,26 @@
+﻿using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MS.Commands.AR
+{
+    public class LintelsSections : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            //Категория == Обобщенная модель
+            //Описание == Перемычка
+            //PGS_МаркаПеремычки-- имеет значение
+
+            //Element.Location - точка размещения(X Y координаты)
+            //FamilyInstance.HandOrientation - вектор вдоль длины перемычки
+            //FamilyInstance.GetSubComponentIds().GetFirst().Location.Z - отметка центра разреза по высоте.
+
+            throw new NotImplementedException();
+        }
+    }
+}
