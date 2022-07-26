26ca8f3a8ea4a2fbe515c2c43e706dcf7805fe6a
Vladimir Stroganov <vlastroganov@gmail.com>
2022-07-26 15:46:13 +0300

    Создание изображений из разрезов

--- Commands/AR/CreateImagesFromSections.cs
+++ Commands/AR/CreateImagesFromSections.cs
@@ -1,11 +1,13 @@
 ﻿using Autodesk.Revit.Attributes;
 using Autodesk.Revit.DB;
 using Autodesk.Revit.UI;
+using MS.Shared;
 using MS.Utilites;
 using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Windows;
 
 namespace MS.Commands.AR
 {
@@ -26,27 +28,126 @@ namespace MS.Commands.AR
         /// </summary>
         private string dirPath = String.Empty;
 
-        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        /// <summary>
+        /// Значение встроенного параметра типа "Описание" для семейств перемычек
+        /// </summary>  
+        private string _lintelDescription = "Перемычка";
+
+        private DirectoryInfo CreateDirAndImgs(
+            ExternalCommandData commandData,
+            List<ElementId> sections)
         {
             UIApplication uiapp = commandData.Application;
             UIDocument uidoc = uiapp.ActiveUIDocument;
             Document doc = uidoc.Document;
 
+            View activeViewWhenCommandStarted = uiapp.ActiveUIDocument.ActiveView;
+
+            ImageExportOptions ieo = new ImageExportOptions()
+            {
+                ExportRange = ExportRange.CurrentView,
+                FitDirection = FitDirectionType.Horizontal,
+                ImageResolution = ImageResolution.DPI_600,
+                ZoomType = ZoomFitType.Zoom,
+                Zoom = 100,
+                HLRandWFViewsFileType = ImageFileType.PNG
+            };
+
+            // Включение весов линий
+            bool thinLines = ThinLinesOptions.AreThinLinesEnabled;
+            if (!thinLines)
+                ThinLinesOptions.AreThinLinesEnabled = true;
+
+            dirPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + @_dirName;
+            DirectoryInfo temporaryFolder = Directory.CreateDirectory(@dirPath);
+            foreach (ElementId section in sections)
+            {
+                View sectionView = doc.GetElement(section) as View;
+                uiapp.ActiveUIDocument.ActiveView = sectionView;
+                ieo.FilePath = dirPath + sectionView.Name;
+                doc.ExportImage(ieo);
+                if (sectionView != activeViewWhenCommandStarted)
+                {
+                    // Закрыть открытый вид
+                }
+            }
+
+            if (activeViewWhenCommandStarted.ViewType != ViewType.Internal
+                && activeViewWhenCommandStarted.ViewType != ViewType.Undefined)
+            {
+                uiapp.ActiveUIDocument.ActiveView = activeViewWhenCommandStarted;
+            }
+
+            // Возвращение весов линий в изначальное состояние
+            if (!thinLines)
+                ThinLinesOptions.AreThinLinesEnabled = false;
+            return temporaryFolder;
+        }
+
+        private void LoadImgsFromDirToDocument(
+            Document doc,
+            DirectoryInfo temporaryFolder,
+            IEnumerable<Element> createdImages,
+            IEnumerable<string> createdImagesNames,
+            ref int loadedImgs,
+            ref int updatedImgs)
+        {
+            var files = temporaryFolder.GetFiles("ПР*.png");
+
+            foreach (FileInfo file in files)
+            {
+                bool isDocContainsImage = createdImagesNames.Contains(file.Name);
+                ImageTypeOptions ito = new ImageTypeOptions(
+                    file.FullName,
+                    false,
+                    ImageTypeSource.Import);
+
+                // Если документ содержит изображение перемычки
+                if (isDocContainsImage)
+                {
+                    createdImages.Where(i => i.Name == file.Name)
+                        .Cast<ImageType>()
+                        .FirstOrDefault()
+                        .ReloadFrom(ito);
+                    updatedImgs++;
+                }
+                // Если документ не содержит изображение перемычки
+                else
+                {
+                    ImageType.Create(doc, ito);
+                    loadedImgs++;
+                }
+            }
+        }
+
+
+        public Result Execute(
+            ExternalCommandData commandData,
+            ref string message,
+            ElementSet elements)
+        {
+            UIApplication uiapp = commandData.Application;
+            UIDocument uidoc = uiapp.ActiveUIDocument;
+            Document doc = uidoc.Document;
+
+            int updateImgs = 0;
+            int newLoadedImgs = 0;
+            int updatedLintels = 0;
+
             // Созданные разрезы по перемычкам, имена начинаются с "ПР-".
-            FilteredElementCollector sectionsFilter = new FilteredElementCollector(doc);
-            var createdSections = sectionsFilter
+            var createdSections = new FilteredElementCollector(doc)
                 .OfCategory(BuiltInCategory.OST_Views)
                 .WhereElementIsNotElementType()
                 .Where(v => (v as View).ViewType == ViewType.Section)
                 .Where(v => v.Name.StartsWith(_prefix));
 
             // Созданные изображения перемычек, имена начинаются с "ПР-".
-            FilteredElementCollector imagesFilter = new FilteredElementCollector(doc);
-            var createdImagesNames = imagesFilter
-                .OfCategory(BuiltInCategory.OST_RasterImages)
+            var createdImages = new FilteredElementCollector(doc)
+                .OfClass(typeof(ImageType))
                 .WhereElementIsElementType()
-                .Where(v => v.Name.StartsWith(_prefix))
-                .Select(v => v.Name);
+                .Where(v => v.Name.StartsWith(_prefix));
+
+            var createdImagesNames = createdImages.Select(v => v.Name);
 
             List<ElementId> sections;
 
@@ -76,54 +177,73 @@ namespace MS.Commands.AR
                     return Result.Cancelled;
             }
 
-            // Включение весов линий
-            bool thinLines = ThinLinesOptions.AreThinLinesEnabled;
-            if (!thinLines)
-                ThinLinesOptions.AreThinLinesEnabled = true;
-            View activeViewWhenCommandStarted = uiapp.ActiveUIDocument.ActiveView;
+            DirectoryInfo temporaryFolder = CreateDirAndImgs(commandData, sections);
 
-            ImageExportOptions ieo = new ImageExportOptions()
+            using (Transaction transImgsLoading = new Transaction(doc))
             {
-                ExportRange = ExportRange.CurrentView,
-                FitDirection = FitDirectionType.Horizontal,
-                ImageResolution = ImageResolution.DPI_600,
-                ZoomType = ZoomFitType.Zoom,
-                Zoom = 100,
-                HLRandWFViewsFileType = ImageFileType.PNG
-            };
+                transImgsLoading.Start("PGS_LoadLintelsImgs");
 
-            dirPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + @_dirName;
-            Directory.CreateDirectory(@dirPath);
-            foreach (ElementId section in sections)
-            {
-                View sectionView = doc.GetElement(section) as View;
-                uiapp.ActiveUIDocument.ActiveView = sectionView;
+                LoadImgsFromDirToDocument(
+                    doc,
+                    temporaryFolder,
+                    createdImages,
+                    createdImagesNames,
+                    ref newLoadedImgs,
+                    ref updateImgs);
 
-                ieo.FilePath = dirPath + sectionView.Name;
-                string test = ieo.FilePath;
-                doc.ExportImage(ieo);
-                if (sectionView != activeViewWhenCommandStarted)
-                {
-                    // Закрыть открытый вид
-                }    
+                transImgsLoading.Commit();
             }
-            uiapp.ActiveUIDocument.ActiveView = activeViewWhenCommandStarted;
-            //using (Transaction trans = new Transaction(doc))
-            //{
-            //    trans.Start("Изображения перемычек");
-            //    Directory.CreateDirectory(@dirPath);
-            //    ieo.FilePath = dirPath;
-            //    doc.ExportImage(ieo);
 
-            //    trans.Commit();
-            //}
+            var lintels = new FilteredElementCollector(doc)
+                .OfCategory(BuiltInCategory.OST_GenericModel)
+                .WhereElementIsNotElementType()
+                .Where(e => (e is FamilyInstance) &&
+                            (e as FamilyInstance).Symbol
+                            .get_Parameter(BuiltInParameter.ALL_MODEL_DESCRIPTION)
+                            .AsValueString() == _lintelDescription)
+                .Where(e => e.get_Parameter(SharedParams.PGS_MarkLintel) != null &&
+                            !String.IsNullOrEmpty(e.get_Parameter(SharedParams.PGS_MarkLintel).AsValueString()))
+                .Cast<FamilyInstance>();
 
+            var loadedImgs = new FilteredElementCollector(doc)
+                .OfClass(typeof(ImageType))
+                .WhereElementIsElementType()
+                .Where(v => v.Name.StartsWith(_prefix));
 
+            using (Transaction transSetImgs = new Transaction(doc))
+            {
+                transSetImgs.Start("PGS_SetLintelsImgs");
 
+                foreach (FamilyInstance lintel in lintels)
+                {
+                    string lintelMarkImg = lintel.get_Parameter(SharedParams.PGS_MarkLintel)
+                        .AsValueString() + ".png";
+                    var lintelImg = lintel.get_Parameter(BuiltInParameter.ALL_MODEL_IMAGE);
+                    if (lintelImg.AsElementId() == null || lintelImg.AsValueString() != lintelMarkImg)
+                    {
+                        Element img = loadedImgs.Where(i => i.Name == lintelMarkImg).FirstOrDefault();
+                        lintel.get_Parameter(BuiltInParameter.ALL_MODEL_IMAGE).Set(img.Id);
+                        updatedLintels++;
+                    }
+                }
 
-            // Возвращение весов линий в изначальное состояние
-            if (!thinLines)
-                ThinLinesOptions.AreThinLinesEnabled = false;
+                transSetImgs.Commit();
+            }
+
+            temporaryFolder.Delete(true);
+
+            MessageBox.Show(
+                $"Загружено {newLoadedImgs} изображений перемычек;" +
+                $"\nОбновлено {updateImgs} изображений перемычек;" +
+                $"\nНазначено {updatedLintels} изображений перемычкам." +
+                $"\n\nПримечание:" +
+                $"\nИзображения назначаются только тем перемычкам," +
+                $"\nу которых задан параметр PGS_МаркаПеремычки" +
+                $"\nи для которых созданы разрезы." +
+                $"\nЕсли название изображения совпадает с PGS_МаркаПеремычки," +
+                $"\nто переназначаться перемычке оно не будет.",
+                "Изображения перемычек");
+
 
             return Result.Succeeded;
         }
---
+++ Shared/SharedParams.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MS.Shared
+{
+    public static class SharedParams
+    {
+        /// <summary>
+        /// Guid параметра PGS_МаркаПеремычки
+        /// </summary>
+        public static readonly Guid PGS_MarkLintel = Guid.Parse("aee96840-3b85-4cb6-a93e-85acee0be8c7");
+
+    }
+}
