d1ad3cc6ef87326bda2d04e84a538078ff7d6b09
Vladimir Stroganov <vlastroganov@gmail.com>
2022-08-25 10:07:08 +0300

    Создать заготовку для команды "Системы в пространствах"

---
+++ Commands/MEP/SystemsInSpace.cs
@@ -0,0 +1,14 @@
+﻿using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using System;
+
+namespace MS.Commands.MEP
+{
+    public class SystemsInSpace : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
