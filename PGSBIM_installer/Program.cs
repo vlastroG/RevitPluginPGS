6226885d44b5f625604cb7bc22ab89e0a2aaf4db
Vladimir Stroganov <vlastroganov@gmail.com>
2022-10-02 14:01:36 +0300

    Скорректировать installer PGS-BIM

--- PGSBIM_installer/Program.cs
+++ PGSBIM_installer/Program.cs
@@ -4,12 +4,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.IO;
+using System.Timers;
 
 namespace PGSBIM_installer
 {
     internal class Program
     {
-        private static string _pluginDir = @"Q:\!Ресурсы\!PGS-BIM_Plugin";
+        private static string _pluginDir = @"Q:\!Ресурсы\!PGS-BIM_Plugin\PGS-BIM";
 
         private static string _destDir = @"C:\ProgramData\Autodesk\Revit\Addins\2022";
 
@@ -34,23 +35,23 @@ namespace PGSBIM_installer
             {
                 return;
             }
-            Console.WriteLine($"Changed: {e.FullPath}");
+            Console.WriteLine($"--> Changed: {e.FullPath}");
         }
 
         private static void OnCreated(object sender, FileSystemEventArgs e)
         {
-            string value = $"Created: {e.FullPath}";
+            string value = $"--> Created: {e.FullPath}";
             Console.WriteLine(value);
         }
 
         private static void OnDeleted(object sender, FileSystemEventArgs e) =>
-            Console.WriteLine($"Deleted: {e.FullPath}");
+            Console.WriteLine($"--> Deleted: {e.FullPath}");
 
         private static void OnRenamed(object sender, RenamedEventArgs e)
         {
-            Console.WriteLine($"Renamed:");
-            Console.WriteLine($"    Old: {e.OldFullPath}");
-            Console.WriteLine($"    New: {e.FullPath}");
+            Console.WriteLine($"--> Renamed:");
+            Console.WriteLine($"-->     Old: {e.OldFullPath}");
+            Console.WriteLine($"-->     New: {e.FullPath}");
         }
 
         private static void OnError(object sender, ErrorEventArgs e) =>
@@ -60,18 +61,60 @@ namespace PGSBIM_installer
         {
             if (ex != null)
             {
-                Console.WriteLine($"Message: {ex.Message}");
-                Console.WriteLine("Stacktrace:");
+                Console.WriteLine($"--> Message: {ex.Message}");
+                Console.WriteLine("--> Stacktrace:");
                 Console.WriteLine(ex.StackTrace);
                 Console.WriteLine();
                 PrintException(ex.InnerException);
+                Console.ReadKey(true);
             }
         }
 
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Установка плагина PGS-BIM...");
-            CopyFilesRecursively(_pluginDir, _destDir);
+            Console.WriteLine("Установка плагина PGS-BIM...\n");
+            using (var watcher = new FileSystemWatcher(_destDir))
+            {
+                watcher.NotifyFilter = NotifyFilters.Attributes
+                                     | NotifyFilters.CreationTime
+                                     | NotifyFilters.DirectoryName
+                                     | NotifyFilters.FileName
+                                     | NotifyFilters.LastAccess
+                                     | NotifyFilters.LastWrite
+                                     | NotifyFilters.Security
+                                     | NotifyFilters.Size;
+
+                watcher.Changed += OnChanged;
+                watcher.Created += OnCreated;
+                watcher.Deleted += OnDeleted;
+                watcher.Renamed += OnRenamed;
+                watcher.Error += OnError;
+
+                watcher.Filter = "*.*";
+                watcher.IncludeSubdirectories = true;
+                watcher.EnableRaisingEvents = true;
+                bool exceptionThrown = false;
+                try
+                {
+                    CopyFilesRecursively(_pluginDir, _destDir);
+                }
+                catch (Exception)
+                {
+                    exceptionThrown = true;
+                    Console.WriteLine();
+                    Console.WriteLine("Ошибка: закройте Revit.");
+                    Console.WriteLine("Нажмите Enter для выхода из установщика");
+                    Console.ReadKey(true);
+                }
+                if (!exceptionThrown)
+                {
+                    Task.Delay(500).Wait();
+                    Console.WriteLine();
+                    Console.WriteLine("Готово, нажмите Enter");
+                    Console.ReadKey(true);
+                }
+            }
         }
     }
 }
