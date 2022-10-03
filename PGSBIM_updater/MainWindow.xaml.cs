d879720cef04fb919faa37b2f4b38560c3c02091
Vladimir Stroganov <vlastroganov@gmail.com>
2022-10-03 15:48:31 +0300

    Создать тестовую версию background updater notifier

---
+++ PGSBIM_updater/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PGSBIM_updater
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public string Status { get { return PGSBIM_updater.ViewModels.Base.UpdaterNotifier.Status; } }
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Load(object sender, RoutedEventArgs e)
+        {
+            Thread updaterBackroundThread = new Thread(PGSBIM_updater.ViewModels.Base.UpdaterNotifier.Run);
+            updaterBackroundThread.Start();
+        }
+    }
+}
---
+++ PGSBIM_updater/ViewModels/Base/UpdaterNotifier.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace PGSBIM_updater.ViewModels.Base
+{
+    internal static class UpdaterNotifier
+    {
+        public static string Status { get; private set; }
+
+        private static readonly string _pathDest = @"C:\Users\stroganov.vg\Documents\TEST\test.txt";
+        private static readonly string _pathSource = @"C:\Users\stroganov.vg\Documents\TEST_source\test.txt";
+
+
+        private static readonly string _pathTestOut = @"C:\Users\stroganov.vg\Documents\TEST_source\testOut.txt";
+
+
+        //public static void Run(CancellationToken cancellationToken)
+        public static void Run()
+        {
+
+            //while (!cancellationToken.IsCancellationRequested)
+            while (true)
+            {
+                Thread.Sleep(30000);
+                if (!File.Exists(_pathSource))
+                {
+                    Status = "Source отсутствует";
+                    MessageBox.Show($"{Status}", "PGS-BIM обновление");
+
+                }
+                else
+                {
+                    DateTime dateSource = File.GetLastWriteTime(_pathSource);
+                    DateTime dateDest = File.GetLastWriteTime(_pathDest);
+                    if (dateSource > dateDest)
+                    {
+                        Status = "Есть обновление для плагина. Можете закрыть Revit и обновить плагин вручную.";
+                        MessageBox.Show($"{Status}", "PGS-BIM обновление");
+                    }
+                    else
+                    {
+                        Status = "У Вас самая последняя версия плагина";
+                        MessageBox.Show($"{Status}", "PGS-BIM обновление");
+
+                    }
+                }
+                TestWriteOut();
+            }
+        }
+
+        private static void TestWriteOut()
+        {
+            File.AppendAllText(_pathTestOut, Status);
+        }
+    }
+}
