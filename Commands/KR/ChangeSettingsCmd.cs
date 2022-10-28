c6c193c84d33bf9eee9ab9216e4917caba19ce9c
Vladimir Stroganov <vlastroganov@gmail.com>
2022-10-28 16:37:07 +0300

    Добавить команду для натроек КР и UI для нее

---
+++ Commands/KR/ChangeSettingsCmd.cs
@@ -0,0 +1,25 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using MS.GUI.KR;
+using MS.GUI.ViewModels.KR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MS.Commands.KR
+{
+    [Transaction(TransactionMode.Manual)]
+    [Regeneration(RegenerationOption.Manual)]
+    public class ChangeSettingsCmd : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            var settingsKRui = new SettingsKRview();
+            settingsKRui.ShowDialog();
+            return Result.Succeeded;
+        }
+    }
+}
---
+++ GUI/KR/SettingsKRview.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MS.GUI.KR
+{
+    /// <summary>
+    /// Interaction logic for SettingsKRview.xaml
+    /// </summary>
+    public partial class SettingsKRview : Window
+    {
+        public SettingsKRview()
+        {
+            InitializeComponent();
+        }
+
+        private void NumberValidationTextBox(object sender, TextCompositionEventArgs e)
+        {
+            Regex regex = new Regex("[^0-9]+");
+            e.Handled = regex.IsMatch(e.Text);
+        }
+
+        private void OkButton_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = true;
+        }
+
+        private void CancelButton_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = false;
+        }
+    }
+}
