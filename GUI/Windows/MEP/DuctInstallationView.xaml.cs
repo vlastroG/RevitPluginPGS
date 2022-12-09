e555f8b422ef10f87ebcd566e44882b0317b5952
Vladimir Stroganov <vlastroganov@gmail.com>
2022-12-09 17:58:34 +0300

    Создание заготовки главного окна настроек

---
+++ GUI/Windows/MEP/DuctInstallationView.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MS.GUI.Windows.MEP
+{
+    /// <summary>
+    /// Interaction logic for DuctInstallationView.xaml
+    /// </summary>
+    public partial class DuctInstallationView : Window
+    {
+        public DuctInstallationView()
+        {
+            InitializeComponent();
+        }
+    }
+}
