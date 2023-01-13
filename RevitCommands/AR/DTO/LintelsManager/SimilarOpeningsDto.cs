647652d1a5dbe3edc7b6826847a5453e628bd485
Vladimir Stroganov <vlastroganov@gmail.com>
2023-01-13 18:02:35 +0300

    Заготовка для группировки похожих проемов

---
+++ RevitCommands/AR/DTO/LintelsManager/SimilarOpeningsDto.cs
@@ -0,0 +1,14 @@
+﻿using MS.GUI.ViewModels.Base;
+using MS.RevitCommands.Models.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MS.RevitCommands.AR.DTO.LintelsManager
+{
+    public class SimilarOpeningsDto
+    {
+    }
+}
